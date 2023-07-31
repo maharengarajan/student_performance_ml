3addd95f3a2d98d8c40f425ce1dc27b806f8399c
RENGA RAJAN K <maharengarajan46@gmail.com>
2023-07-31 15:07:45 +0530

    Flask API building

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from typing import List
 
 HYPEN_E_DOT = '-e .'
 def get_requirements(file_path:str)->List[str]:
+    '''This fun will return the list of requirements'''
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
