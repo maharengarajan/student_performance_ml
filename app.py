4bb393c5043ed8eff438a1b9e62c7ba17c2a3681
RENGA RAJAN K <maharengarajan46@gmail.com>
2023-08-16 17:57:34 +0530

    app.py tested

--- app.py
+++ app.py
@@ -10,23 +10,23 @@ app = Flask(__name__)
 #Route for a home page
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html') 
 
 @app.route('/predictdata', methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
     else:
-        data = CustomData(
-            gender = request.form.get('gender'),
-            race_ethnicity=request.form.get('race_ethnicity'),
+        data=CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
             parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
             reading_score=float(request.form.get('reading_score')),
             writing_score=float(request.form.get('writing_score'))
-        )
 
+        )
         pred_df=data.get_data_as_data_frame()
         print(pred_df)
         print("Before Prediction")
@@ -39,5 +39,4 @@ def predict_datapoint():
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0",debug=True)   
-
+    app.run(host="0.0.0.0", debug=True)        
