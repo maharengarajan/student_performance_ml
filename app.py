b398abe64867d09735fe55ff8e4de06c3d2e0c98
RENGA RAJAN K <maharengarajan46@gmail.com>
2023-08-17 10:39:06 +0530

    app.py modified

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
-app = Flask(__name__)
+application = Flask(__name__)
+app = application
 
 #Route for a home page
 @app.route('/')
@@ -29,14 +30,14 @@ def predict_datapoint():
         )
         pred_df=data.get_data_as_data_frame()
         print(pred_df)
-        print("Before Prediction")
+        #print("Before Prediction")
 
         predict_pipeline=PredictPipeline()
-        print("Mid Prediction")
+        #print("Mid Prediction")
         results=predict_pipeline.predict(pred_df)
-        print("after Prediction")
+        #print("after Prediction")
         return render_template('home.html',results=results[0])
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0", debug=True)        
+    app.run(host="0.0.0.0")        
